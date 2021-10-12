343c9f1cb70b1677d55e133122328785e8c84769
Scott Lind <scttlnd@gmail.com>
2021-10-12 16:40:25 +0200

    Modal refactoring

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <button class="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" v-on:click="toggleModal()">
+    <button class="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" 
+    type="button" v-on:click="toggleModal()">
       View high resolution image
     </button>
     <div v-if="showModal" class="h-screen p-5 overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
@@ -8,10 +9,7 @@
         <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
           <!--header-->
           <div class="flex items-start justify-between p-3 border-b border-solid border-gray-200 rounded-t">
-            <!-- <h3 class="text-3xl font-semibold">
-              Modal Title
-            </h3> -->
-            <button class="p-1 ml-auto bg-transparent border-0 text-red opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" v-on:click="toggleModal()">
+            <button class="p-1 ml-auto bg-transparent border-0 text-red opacity-5 float-right text-3xl leading-none font-bold outline-none focus:outline-none" v-on:click="toggleModal()">
               <span class="text-red text-3xl block outline-red focus:outline-red">
                 Close
               </span>
@@ -19,14 +17,8 @@
           </div>
           <!--body-->
           <div class="relative p-6 flex-auto">
-            <img class="" alt="hero" :src="apodDataObj.hdurl">
+            <img :alt="apod.title" :src="apod.hdurl">
           </div>
-          <!--footer-->
-          <!-- <div class="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-            <button class="text-red-500 bg-transparent border border-solid border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" v-on:click="toggleModal()">
-              Close
-            </button>
-          </div> -->
         </div>
       </div>
     </div>
--- src/components/AppPhoto.vue
+++ src/components/AppPhoto.vue
@@ -1,7 +1,7 @@
 <template>
 <section class="text-gray-600 body-font">
   <div class="container mx-auto flex p-5 items-center justify-center flex-col">
-    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" :src="apod.url">
+    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" :alt="apod.title" :src="apod.url">
     <div class="text-center lg:w-2/3 w-full">
       <h1 class="title-font sm:text-4xl text-3xl mb-2 font-medium text-gray-900">{{ apod.title }}</h1>
       <h2 class="title-font sm:text-xl text-l mb-4 font-medium text-gray-500">{{ apod.copyright }}</h2>
